50535e5173878ec1957069b58e0ba7b6e200b236
DESKTOP-HHQC8A8\PC <triet.ngo160208@vnuk.edu.vn>
2018-11-14 20:38:46 +0700

    migrate database

---
+++ database/migrations/2018_11_14_132449_create_songs_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSongsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('songs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('description');
+            $table->string('file_name');
+            $table->string('cover_image');
+            $table->integer('year');
+            $table->string('genre');
+            $table->integer('composer_id')->index();
+            $table->integer('singer_id')->index();
+            $table->integer('user_id')->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('songs');
+    }
+}
