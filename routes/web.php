e5d0a40a10a94bb9835eff284219ec42afc515ff
DESKTOP-HHQC8A8\PC <triet.ngo160208@vnuk.edu.vn>
2018-11-17 08:53:55 +0700

    login and register

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,9 @@ Route::get('/', function () {
 Route::prefix('user')->group(function () {
     Route::get('/', 'UserController@index');
 
-    Route::get('/login', 'UserController@login');
+    Route::post('/login', 'UserController@login');
 
-    Route::get('/register', 'UserController@register');
+    Route::post('/register', 'UserController@register');
 });
 
 Auth::routes();
